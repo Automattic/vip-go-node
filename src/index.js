9eb029292dcba299c8322ad336f1fad7539f8f17
Ahmed <elazzabi.ahmed@gmail.com>
2018-08-30 17:40:50 +0100

    Add New Relic support (#23)
    * Add newrelic to project
    
    * Add New Relic support with tests
    
    * Remove production checking and add errors
    
    * Add auto mocking and tests
    
    * Fix eslint errors
    
    * Expose newrelic in package
    
    * Add README to new relic module
    
    * Fix lint error
    
    * Add a top level describe with module name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const logger = require( './logger/' );
 const server = require( './server/' );
+const newrelic = require( './newrelic/' );
 
 module.exports = {
 	logger,
 	server,
+	newrelic,
 };
---
+++ src/newrelic/index.js
@@ -0,0 +1,16 @@
+module.exports = ( { logger = console } = {} ) => {
+	const licenseKey = process.env.NEW_RELIC_LICENSE_KEY;
+	const noConfig = process.env.NEW_RELIC_NO_CONFIG_FILE === 'true';
+
+	if ( ! noConfig ) {
+		throw new Error( `An environment variable is missing 
+			or not set to true: NEW_RELIC_NO_CONFIG_FILE` );
+	}
+
+	if ( ! licenseKey ) {
+		throw new Error( 'An environment variable is missing: NEW_RELIC_LICENSE_KEY' );
+	}
+
+	logger.info( 'Importing New Relic library...' );
+	return require( 'newrelic' );
+};
