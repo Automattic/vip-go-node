14311dc2c311ca9db6b28e70ac08d838ccca6f52
Mohammad Jangda <batmoo@gmail.com>
2018-10-08 12:43:32 -0400

    Throw error if newrelic module doesn't exist
    Removes automock to make it easier to mock per-test.

--- __tests__/newrelic.tests.js
+++ __tests__/newrelic.tests.js
@@ -1,14 +1,21 @@
 const newrelic = require( '../src/newrelic/' );
 
 describe( 'src/newrelic', () => {
-	describe( 'Should fail if some environment variables are not present', () => {
-		const OLD_ENV_VARS = process.env;
+	const OLD_ENV_VARS = process.env;
+	beforeEach( () => {
+		jest.doMock( 'newrelic', () => ( {
+			value: 'newrelic',
+		} ) );
+	} );
 
-		// eslint-disable-next-line no-undef
-		afterEach( () => {
-			process.env = OLD_ENV_VARS;
-		} );
+	// eslint-disable-next-line no-undef
+	afterEach( () => {
+		jest.resetModules();
 
+		process.env = OLD_ENV_VARS;
+	} );
+
+	describe( 'Environment variables are missing', () => {
 		test( 'Should fail if NEW_RELIC_NO_CONFIG_FILE is not set', () => {
 			expect( () => {
 				newrelic();
@@ -32,8 +39,21 @@ describe( 'src/newrelic', () => {
 		} );
 	} );
 
-	describe( 'Should return expected values when all configuration is present', () => {
-		test( 'Should return newrelic module', () => {
+	describe( 'Environment variables are present', () => {
+		test( 'Should fail if `newrelic` module errors out', () => {
+			jest.doMock( 'newrelic', () => {
+				throw new Error( 'Module does not exist.' );
+			} );
+
+			process.env.NEW_RELIC_NO_CONFIG_FILE = true;
+			process.env.NEW_RELIC_LICENSE_KEY = 'ABC';
+
+			expect( () => {
+				newrelic();
+			} ).toThrowError( /could not be imported/ );
+		} );
+
+		test( 'Should return newrelic module when config correct set', () => {
 			process.env.NEW_RELIC_NO_CONFIG_FILE = true;
 			process.env.NEW_RELIC_LICENSE_KEY = 'ABC';
 			const returnedValue = newrelic();
