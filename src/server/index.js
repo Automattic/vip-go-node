397de142313c973627383e0a2f89826b278d2b75
Ahmed <elazzabi.ahmed@gmail.com>
2018-11-16 17:25:40 +0100

    Log when server is listening (#53)
    * add log entry for listening
    
    * check for function

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,13 @@ const wrapApplication = ( application, { PORT, logger } ) => {
 		server,
 		listen: connected => {
 			logger.info( 'Starting server on ' + PORT );
-			server = app.listen( PORT, connected );
+			server = app.listen( PORT, () => {
+				logger.info( 'Server listening on port ' + PORT );
+
+				if ( typeof connected === 'function' ) {
+					connected();
+				}
+			} );
 		},
 		close: () => {
 			server.close();
