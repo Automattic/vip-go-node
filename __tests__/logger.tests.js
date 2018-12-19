da247448e403938a0a210b9a36f9b5eb412a9f12
Ahmed <elazzabi.ahmed@gmail.com>
2018-12-19 10:29:38 +0100

    adds top level describe to logger and server (#65)

--- __tests__/logger.tests.js
+++ __tests__/logger.tests.js
@@ -15,133 +15,135 @@ class TestTransport extends Transport {
 	}
 }
 
-describe( 'Logger should fail if some parameters are not initialized', () => {
-	it( 'Should fail if not initialized with a namespace', () => {
-		expect( () => {
-			goLogger.debug( 'This should fail as namespace is not provided' );
-		} ).toThrow();
-	} );
-} );
-
-describe( 'Logger should format messages and log to the provided transport', () => {
-	it( 'Should log a simple error message', () => {
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport } );
-
-		log.info( 'A simple log' );
-
-		const firstLog = transport.logs[ 0 ];
-
-		expect( firstLog ).toHaveProperty( 'message', 'A simple log' );
-	} );
-
-	it( 'Should format an error message', () => {
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport } );
-
-		log.debug( 'Should format %s message', 'this' );
-
-		const firstLog = transport.logs[ 0 ];
-
-		expect( firstLog ).toHaveProperty( 'message', 'Should format this message' );
-	} );
-
-	describe( 'local logging', () => {
-		it( 'should format output correctly', () => {
-			const transport = new TestTransport();
-			const log = goLogger( 'go:app', { transport } );
-
-			log.info( 'my message' );
-
-			const firstLog = transport.logs[ 0 ];
-
-			const message = firstLog[ symbolForMessage ];
-			// eslint-disable-next-line max-len
-			expect( message ).toEqual( expect.stringMatching( /^\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} GMT go:app \[info\] my message$/ ) );
+describe( 'src/logger', () => {
+	describe( 'Logger should fail if some parameters are not initialized', () => {
+		it( 'Should fail if not initialized with a namespace', () => {
+			expect( () => {
+				goLogger.debug( 'This should fail as namespace is not provided' );
+			} ).toThrow();
 		} );
 	} );
 
-	describe( 'production logging', () => {
-		const ORIGINAL_VIP_GO_APP_ID = process.env.VIP_GO_APP_ID;
-
-		beforeEach( () => process.env.VIP_GO_APP_ID = true );
-		afterEach( () => process.env.VIP_GO_APP_ID = ORIGINAL_VIP_GO_APP_ID );
-
-		it( 'should format output correctly', () => {
+	describe( 'Logger should format messages and log to the provided transport', () => {
+		it( 'Should log a simple error message', () => {
 			const transport = new TestTransport();
-			const log = goLogger( 'go:app', { transport } );
+			const log = goLogger( 'go:application:test', { transport } );
 
-			log.info( 'my message' );
+			log.info( 'A simple log' );
 
 			const firstLog = transport.logs[ 0 ];
 
-			const message = firstLog[ symbolForMessage ];
-			// eslint-disable-next-line max-len
-			expect( message ).toEqual( expect.stringMatching( /^\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} GMT go:app {"message":"my message","level":"info","app":"go","app_type":"app","message_type":"info","app_process":"master","app_worker":"master"}$/ ) );
+			expect( firstLog ).toHaveProperty( 'message', 'A simple log' );
+		} );
+
+		it( 'Should format an error message', () => {
+			const transport = new TestTransport();
+			const log = goLogger( 'go:application:test', { transport } );
+
+			log.debug( 'Should format %s message', 'this' );
+
+			const firstLog = transport.logs[ 0 ];
+
+			expect( firstLog ).toHaveProperty( 'message', 'Should format this message' );
+		} );
+
+		describe( 'local logging', () => {
+			it( 'should format output correctly', () => {
+				const transport = new TestTransport();
+				const log = goLogger( 'go:app', { transport } );
+
+				log.info( 'my message' );
+
+				const firstLog = transport.logs[ 0 ];
+
+				const message = firstLog[ symbolForMessage ];
+				// eslint-disable-next-line max-len
+				expect( message ).toEqual( expect.stringMatching( /^\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} GMT go:app \[info\] my message$/ ) );
+			} );
+		} );
+
+		describe( 'production logging', () => {
+			const ORIGINAL_VIP_GO_APP_ID = process.env.VIP_GO_APP_ID;
+
+			beforeEach( () => process.env.VIP_GO_APP_ID = true );
+			afterEach( () => process.env.VIP_GO_APP_ID = ORIGINAL_VIP_GO_APP_ID );
+
+			it( 'should format output correctly', () => {
+				const transport = new TestTransport();
+				const log = goLogger( 'go:app', { transport } );
+
+				log.info( 'my message' );
+
+				const firstLog = transport.logs[ 0 ];
+
+				const message = firstLog[ symbolForMessage ];
+				// eslint-disable-next-line max-len
+				expect( message ).toEqual( expect.stringMatching( /^\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} GMT go:app {"message":"my message","level":"info","app":"go","app_type":"app","message_type":"info","app_process":"master","app_worker":"master"}$/ ) );
+			} );
 		} );
 	} );
-} );
 
-describe( 'Logger should add necessary labels and handle custom ones', () => {
-	it( 'Should add custom labels to the output', () => {
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport } );
+	describe( 'Logger should add necessary labels and handle custom ones', () => {
+		it( 'Should add custom labels to the output', () => {
+			const transport = new TestTransport();
+			const log = goLogger( 'go:application:test', { transport } );
 
-		log.error( 'Should add my custom label', { customLabel: 'custom value' } );
+			log.error( 'Should add my custom label', { customLabel: 'custom value' } );
 
-		const firstLog = transport.logs[ 0 ];
+			const firstLog = transport.logs[ 0 ];
 
-		expect( firstLog ).toHaveProperty( 'customLabel', 'custom value' );
+			expect( firstLog ).toHaveProperty( 'customLabel', 'custom value' );
+		} );
+
+		it( 'Should format and add new labels to the output', () => {
+			const transport = new TestTransport();
+			const log = goLogger( 'go:application:test', { transport } );
+
+			log.error( 'Should format %s, and add my custom label', 'this',
+				{ customLabel: 'custom value' }
+			);
+
+			const firstLog = transport.logs[ 0 ];
+			const expectedMessage = 'Should format this, and add my custom label';
+
+			expect( firstLog ).toHaveProperty( 'message', expectedMessage );
+			expect( firstLog ).toHaveProperty( 'customLabel', 'custom value' );
+		} );
+
+		it( 'Should include all necessary labels', () => {
+			const transport = new TestTransport();
+			const log = goLogger( 'go:application:test', { transport } );
+
+			log.error( 'Should have some necessary labels' );
+
+			const firstLog = transport.logs[ 0 ];
+
+			expect( firstLog ).toHaveProperty( 'message', 'Should have some necessary labels' );
+			expect( firstLog ).toHaveProperty( 'app', 'go' );
+			expect( firstLog ).toHaveProperty( 'app_type', 'application:test' );
+			expect( firstLog ).toHaveProperty( 'message_type', 'error' );
+			expect( firstLog ).toHaveProperty( 'app_process', 'master' );
+			expect( firstLog ).toHaveProperty( 'app_worker', 'master' );
+		} );
 	} );
 
-	it( 'Should format and add new labels to the output', () => {
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport } );
+	describe( 'Logger should work in a cluster environments', () => {
+		it( 'Should add worker info', () => {
+			const mockedCluster = {
+				isWorker: true,
+				worker: {
+					id: 1234,
+				},
+			};
 
-		log.error( 'Should format %s, and add my custom label', 'this',
-			{ customLabel: 'custom value' }
-		);
+			const transport = new TestTransport();
+			const log = goLogger( 'go:application:test', { transport, cluster: mockedCluster } );
 
-		const firstLog = transport.logs[ 0 ];
-		const expectedMessage = 'Should format this, and add my custom label';
+			log.info( 'Logging from worker' );
 
-		expect( firstLog ).toHaveProperty( 'message', expectedMessage );
-		expect( firstLog ).toHaveProperty( 'customLabel', 'custom value' );
-	} );
+			const firstLog = transport.logs[ 0 ];
 
-	it( 'Should include all necessary labels', () => {
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport } );
-
-		log.error( 'Should have some necessary labels' );
-
-		const firstLog = transport.logs[ 0 ];
-
-		expect( firstLog ).toHaveProperty( 'message', 'Should have some necessary labels' );
-		expect( firstLog ).toHaveProperty( 'app', 'go' );
-		expect( firstLog ).toHaveProperty( 'app_type', 'application:test' );
-		expect( firstLog ).toHaveProperty( 'message_type', 'error' );
-		expect( firstLog ).toHaveProperty( 'app_process', 'master' );
-		expect( firstLog ).toHaveProperty( 'app_worker', 'master' );
-	} );
-} );
-
-describe( 'Logger should work in a cluster environments', () => {
-	it( 'Should add worker info', () => {
-		const mockedCluster = {
-			isWorker: true,
-			worker: {
-				id: 1234,
-			},
-		};
-
-		const transport = new TestTransport();
-		const log = goLogger( 'go:application:test', { transport, cluster: mockedCluster } );
-
-		log.info( 'Logging from worker' );
-
-		const firstLog = transport.logs[ 0 ];
-
-		expect( firstLog ).toHaveProperty( 'app_worker', 'worker_1234' );
+			expect( firstLog ).toHaveProperty( 'app_worker', 'worker_1234' );
+		} );
 	} );
 } );
--- __tests__/server.tests.js
+++ __tests__/server.tests.js
@@ -3,108 +3,110 @@ const expressApp = require( 'express' )();
 const request = require( 'supertest' );
 const HEALTHCHECKURL = '/cache-healthcheck?';
 
-describe( 'Should work with an express application', () => {
-	it( 'Should add a /cache-healthcheck? route returning 200 OK', done => {
-		const expressServer = server( expressApp );
-		request( expressServer.app ).get( HEALTHCHECKURL ).then( response => {
-			expect( response.statusCode ).toBe( 200 );
-			expect( response.text ).toBe( 'ok' );
-			done();
+describe( 'src/server', () => {
+	describe( 'Should work with an express application', () => {
+		it( 'Should add a /cache-healthcheck? route returning 200 OK', done => {
+			const expressServer = server( expressApp );
+			request( expressServer.app ).get( HEALTHCHECKURL ).then( response => {
+				expect( response.statusCode ).toBe( 200 );
+				expect( response.text ).toBe( 'ok' );
+				done();
+			} );
+		} );
+
+		it( 'Should keep already defined routes', done => {
+			expressApp.get( '/down', ( req, res ) => {
+				res.status( 501 ).end();
+			} );
+
+			const expressServer = server( expressApp );
+			request( expressServer.app ).get( '/down' ).then( response => {
+				expect( response.statusCode ).toBe( 501 );
+				done();
+			} );
+		} );
+
+		it( 'Should boot up a server on the provided PORT', done => {
+			const expressServerOnPort = server( expressApp, { PORT: 8000 } );
+			expressServerOnPort.listen();
+			request( 'http://localhost:8000' ).get( HEALTHCHECKURL ).then( response => {
+				expect( response.statusCode ).toBe( 200 );
+				expressServerOnPort.close();
+				done();
+			} );
 		} );
 	} );
 
-	it( 'Should keep already defined routes', done => {
-		expressApp.get( '/down', ( req, res ) => {
-			res.status( 501 ).end();
-		} );
+	describe( 'Should work with a custom request handler', () => {
+		const mock = jest.fn();
+		const requestHandler = ( req, res ) => {
+			if ( req.url === '/custom' ) {
+				res.writeHead( 201 );
+				res.end();
+			}
 
-		const expressServer = server( expressApp );
-		request( expressServer.app ).get( '/down' ).then( response => {
-			expect( response.statusCode ).toBe( 501 );
-			done();
-		} );
-	} );
+			mock();
 
-	it( 'Should boot up a server on the provided PORT', done => {
-		const expressServerOnPort = server( expressApp, { PORT: 8000 } );
-		expressServerOnPort.listen();
-		request( 'http://localhost:8000' ).get( HEALTHCHECKURL ).then( response => {
-			expect( response.statusCode ).toBe( 200 );
-			expressServerOnPort.close();
-			done();
-		} );
-	} );
-} );
-
-describe( 'Should work with a custom request handler', () => {
-	const mock = jest.fn();
-	const requestHandler = ( req, res ) => {
-		if ( req.url === '/custom' ) {
-			res.writeHead( 201 );
+			res.writeHead( 404 );
 			res.end();
-		}
+		};
 
-		mock();
+		afterEach( () => mock.mockClear() );
 
-		res.writeHead( 404 );
-		res.end();
-	};
-
-	afterEach( () => mock.mockClear() );
-
-	it( 'Should raise an error if no request handler is passed', () => {
-		expect( () => {
-			server();
-		} ).toThrow();
-	} );
-
-	it( 'Should add a /cache-healthcheck? route returning 200 OK', done => {
-		const httpServer = server( requestHandler );
-
-		request( httpServer.app ).get( HEALTHCHECKURL ).then( response => {
-			expect( response.statusCode ).toBe( 200 );
-			expect( response.text ).toBe( 'ok' );
-			done();
+		it( 'Should raise an error if no request handler is passed', () => {
+			expect( () => {
+				server();
+			} ).toThrow();
 		} );
-	} );
 
-	it( 'Should respond to /cache-healthcheck? route and not forward the request', done => {
-		const httpServer = server( requestHandler );
+		it( 'Should add a /cache-healthcheck? route returning 200 OK', done => {
+			const httpServer = server( requestHandler );
 
-		request( httpServer.app ).get( HEALTHCHECKURL ).then( response => {
-			expect( response.statusCode ).toBe( 200 );
-			expect( response.text ).toBe( 'ok' );
-			expect( mock ).not.toHaveBeenCalled();
-
-			done();
+			request( httpServer.app ).get( HEALTHCHECKURL ).then( response => {
+				expect( response.statusCode ).toBe( 200 );
+				expect( response.text ).toBe( 'ok' );
+				done();
+			} );
 		} );
-	} );
 
-	it( 'Should match defined routes', done => {
-		const httpServer = server( requestHandler );
+		it( 'Should respond to /cache-healthcheck? route and not forward the request', done => {
+			const httpServer = server( requestHandler );
 
-		request( httpServer.app ).get( '/custom' ).then( response => {
-			expect( response.statusCode ).toBe( 201 );
-			done();
+			request( httpServer.app ).get( HEALTHCHECKURL ).then( response => {
+				expect( response.statusCode ).toBe( 200 );
+				expect( response.text ).toBe( 'ok' );
+				expect( mock ).not.toHaveBeenCalled();
+
+				done();
+			} );
 		} );
-	} );
 
-	it( 'Should return default response if no route is matched', done => {
-		const httpServer = server( requestHandler );
+		it( 'Should match defined routes', done => {
+			const httpServer = server( requestHandler );
 
-		request( httpServer.app ).get( '/notfound' ).then( response => {
-			expect( response.statusCode ).toBe( 404 );
-			done();
+			request( httpServer.app ).get( '/custom' ).then( response => {
+				expect( response.statusCode ).toBe( 201 );
+				done();
+			} );
 		} );
-	} );
 
-	it( 'Should boot up a server on the provided PORT', done => {
-		const httpServerOnPort = server( requestHandler, { PORT: 8000 } );
-		httpServerOnPort.listen();
-		request( 'http://localhost:8000' ).get( HEALTHCHECKURL ).then( response => {
-			expect( response.statusCode ).toBe( 200 );
-			httpServerOnPort.close();
-			done();
+		it( 'Should return default response if no route is matched', done => {
+			const httpServer = server( requestHandler );
+
+			request( httpServer.app ).get( '/notfound' ).then( response => {
+				expect( response.statusCode ).toBe( 404 );
+				done();
+			} );
+		} );
+
+		it( 'Should boot up a server on the provided PORT', done => {
+			const httpServerOnPort = server( requestHandler, { PORT: 8000 } );
+			httpServerOnPort.listen();
+			request( 'http://localhost:8000' ).get( HEALTHCHECKURL ).then( response => {
+				expect( response.statusCode ).toBe( 200 );
+				httpServerOnPort.close();
+				done();
+			} );
 		} );
 	} );
 } );
