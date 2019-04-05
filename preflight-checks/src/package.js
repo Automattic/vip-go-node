fb11c0a28d00fb22c6167d91e996659d2930e61b
Ahmed <elazzabi.ahmed@gmail.com>
2019-04-05 13:00:49 +0100

    add error when folder doesn't contain a package file

--- preflight-checks/src/package.js
+++ preflight-checks/src/package.js
@@ -1,6 +1,14 @@
 const currentWorkingDirectory = process.cwd();
 const path = require( 'path' );
 const packageFile = path.join( currentWorkingDirectory, 'package.json' );
-const packageJson = require( packageFile );
+const chalk = require( 'chalk' );
+
+let packageJson;
+try {
+	packageJson = require( packageFile );
+} catch( e ) {
+	console.error( chalk.red( 'Error:' ), `Could not find a ${ chalk.yellow( 'package.json' ) } in the current folder.` );
+	process.exit( 1 );
+}
 
 module.exports = packageJson;
