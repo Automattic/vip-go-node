5655f13989b4ec4c1930239e35d5f95fecd3f3c0
Ahmed <elazzabi.ahmed@gmail.com>
2019-04-09 11:40:25 +0100

    skip newrelic if local

--- src/newrelic/index.js
+++ src/newrelic/index.js
@@ -2,6 +2,13 @@ module.exports = ( { logger = console } = {} ) => {
 	const licenseKey = process.env.NEW_RELIC_LICENSE_KEY;
 	const noConfig = process.env.NEW_RELIC_NO_CONFIG_FILE === 'true';
 
+	const isLocal = ! process.env.VIP_GO_APP_ID;
+
+	if ( isLocal ) {
+		logger.log( 'Local development, skipping Newrelic initialization...' );
+		return;
+	}
+
 	if ( ! noConfig ) {
 		throw new Error( `An environment variable is missing 
 			or not set to true: NEW_RELIC_NO_CONFIG_FILE` );
