585da2af7791fa46659505b2193f25381aae01a2
Loo3 <jyl2171@columbia.edu>
2019-04-09 15:19:24 -0700

    Set max retries for offline queue to an env variable so clients can designate the #

--- src/redis/index.js
+++ src/redis/index.js
@@ -3,3 +3,6 @@
  */
 const redis = require( 'ioredis' );
 const { logger } = require( '../logger/index' );
+
+//allow clients to set # of queues or default to 3
+const MAX_CONNECTION_RETRIES_FOR_OFFLINE_QUEUE = process.env.MAX_CONNECTIONS_OFFLINE_QUEUE || 3;
