6bb3e8d344c5677ae5b89203845ea6d4bc803abc
Loo3 <jyl2171@columbia.edu>
2019-06-14 00:30:48 -0700

    Reconnect to Redis client if connection is lost and handle offline queues

--- src/redis/index.js
+++ src/redis/index.js
@@ -8,9 +8,6 @@ const IORedis = require( 'ioredis' );
  */
 let { logger } = require( '../logger/index' );
 
-//allow clients to set # of queues or default to 3
-const QUEUED_CONNECTION_ATTEMPTS = process.env.QUEUED_CONNECTION_ATTEMPTS || 3;
-
 //create a class in case we need to scale the connections
 class Redis {
 	constructor() {
@@ -20,17 +17,19 @@ class Redis {
 			port,
 			host,
 			password: process.env.REDIS_PASSWORD,
-			retryStrategy: this.retry.bind( this ),
+			retryStrategy: this.reconnect.bind( this ),
 			enableOfflineQueue: true,
-			maxOfflineQueueSize: Infinity,
+			maxRetriesPerRequest: process.env.QUEUED_CONNECTION_ATTEMPTS || 3,
 		}, { logger = console } = {} );
 
 		this.connect();
-		this.handleEvents();
+		this.reconnectToClient();
 	}
 
-	retry( times ) {
-		return Math.min( times * 250 * 1.6, 5000 );
+	// Wait 2 seconds maximum before attempting reconnection
+	reconnect( times ) {
+		const delay = Math.min( times * 50, 2000 );
+		return delay;
 	}
 
 	// Connect to the Redis client
@@ -41,22 +40,15 @@ class Redis {
 		} );
 	}
 
-	//handle offline queue limits
-	handleEvents() {
-		this.client.on( 'reconnecting', info => {
+	// Reconnect to client if connection is lost and handle offline queue limits
+	reconnectToClient() {
+		this.client.on( 'reconnecting', () => {
 			logger.info( 'Redis is attempting to reconnect' );
-			if ( QUEUED_CONNECTION_ATTEMPTS === ( info.attempt + 1 ) ) {
-				logger.warn( 'There is a problem with the Redis server- disabling offline Redis queue and calling client.flush_and_error()' );
+
+			if ( this.client.maxRetriesPerRequest ) {
+				logger.error( `Max retries reached (max: ${ this.client.maxRetriesPerRequest }). Flushing all pending commands and disabling the offline queue` );
 
 				this.client.enableOfflineQueue = false;
-
-				// Return an error callback to any commands that were previously queued (but only once)
-				// This is kinda hacky as node_redis doesn't expose a way to flush the queue
-				// only, so this will emit an error event
-				this.client.flush_and_error( {
-					message: 'Max of ' + MAX_CONNECTION_RETRIES_FOR_OFFLINE_QUEUE + ' Redis connection attempts reached - flushing queued requests.',
-					code: 'MAX_CONNECTION_RETRIES_FOR_OFFLINE_QUEUE',
-				} );
 			}
 		} );
 	}
