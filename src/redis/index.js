498b2c73638683724213b42b4a4c4c60d9da746b
Ahmed <elazzabi.ahmed@gmail.com>
2020-01-07 14:15:47 +0100

    transform library from class based to function based

--- src/redis/index.js
+++ src/redis/index.js
@@ -3,75 +3,72 @@
  */
 const IORedis = require( 'ioredis' );
 
-let logger = console;
+let redisClient = null;
 
-// Create a class in case we need to scale the connections
-class Redis {
-	constructor() {
-		if ( ! process.env.REDIS_MASTER ) {
-			logger.warn( 'Missing host and port of Redis server' );
-		} else {
-			const [ host, port ] = ( process.env.REDIS_MASTER ).split( ':' );
-
-			this.client = new IORedis( {
-				port,
-				host,
-				password: process.env.REDIS_PASSWORD,
-				retryStrategy: this.reconnect.bind( this ),
-				enableOfflineQueue: true,
-				maxRetriesPerRequest: process.env.QUEUED_CONNECTION_ATTEMPTS || 3,
-			} );
-
-			this.connectToClient();
-			this.reconnectToClient();
-			this.onError();
-			this.disconnect();
-
-			return this.client;
-		}
-	}
-
+const retryStrategy = times => {
 	// Wait 2 seconds maximum before attempting reconnection
-	reconnect( times ) {
-		const delay = Math.min( times * 50, 2000 );
-		return delay;
-	}
-
-	// Connect to the Redis client
-	connectToClient() {
-		this.client.on( 'connect', () => {
-			logger.info( 'Connected to Redis client' );
-			this.client.enableOfflineQueue = true;
-		} );
-	}
-
-	// Reconnect to client if connection is lost and handle offline queue limits
-	reconnectToClient() {
-		this.client.on( 'reconnecting', () => {
-			logger.info( 'Redis is attempting to reconnect' );
-
-			if ( this.client.maxRetriesPerRequest ) {
-				logger.error( 'Max retries reached (max:' + this.client.maxRetriesPerRequest + '). Flushing all pending commands and disabling the offline queue' );
-
-				this.client.enableOfflineQueue = false;
-			}
-		} );
-	}
-
-	// If an error occurs, log the error and expose the error returned from the Redis server
-	onError() {
-		this.client.on( 'error', error => {
-			logger.error( 'Error: ' + error.message + ': ' + JSON.stringify( error ) );
-			error instanceof IORedis.ReplyError;
-		} );
-	}
-
-	// Disconnect from Redis
-	disconnect() {
-		this.client.on( 'disconnect', () => {
-			logger.info( 'Disconnected from Redis client' );
-		} );
-	}
+	return Math.min( times * 50, 2000 );
 }
 
-module.exports = new Redis( { logger = console } = {} );
+module.exports = ( { logger = console } = {} ) => {
+	if( redisClient ) {
+		// Client already defined and initialized
+		return redisClient;
+	}
+
+	const redisMasterIP = process.env.REDIS_MASTER;
+
+	if ( ! redisMasterIP ) {
+		logger.error( `Missing REDIS_MASTER environment variable. Please provide a 
+			REDIS_MASTER environment variable in the form of a host:port string.` );
+		return;
+	}
+
+	const [ host, port ] = redisMasterIP.split( ':' );
+
+	if ( ! host || ! port ) {
+		logger.error( `Couldn't get the host and port from the REDIS_MASTER 
+			environment variable. Please pass a valid REDIS_MASTER environment variable 
+			in the form of a host:port string` );
+		return;
+	}
+
+	logger.info( 'Initializing a new redis client...' );
+
+	redisClient = new IORedis( {
+		host,
+		port,
+		password: process.env.REDIS_PASSWORD,
+		retryStrategy: retryStrategy,
+		enableOfflineQueue: true,
+		maxRetriesPerRequest: process.env.QUEUED_CONNECTION_ATTEMPTS || 3,
+	} );
+
+	// Attaching event listeners
+
+	redisClient.on( 'connect', () => {
+		logger.info( 'Connected to Redis client...' );
+		redisClient.enableOfflineQueue = true;
+	} );
+
+	redisClient.on( 'reconnecting', () => {
+		logger.info( 'Attempting a reconnection to redis...' );
+
+		if ( redisClient.maxRetriesPerRequest ) {
+			logger.error( `Max retries reached (max: ${ redisClient.maxRetriesPerRequest }). 
+				Flushing all pending commands and disabling the offline queue...` );
+
+			redisClient.enableOfflineQueue = false;
+		}
+	} );
+
+	redisClient.on( 'error', error => {
+		logger.error( `Error: ${ error.message }. Complete error: ${ JSON.stringify( error ) }` );
+	} );
+
+	redisClient.on( 'disconnect', () => {
+		logger.info( 'Disconnected from redis client' );
+	} );
+
+	return redisClient;
+}
