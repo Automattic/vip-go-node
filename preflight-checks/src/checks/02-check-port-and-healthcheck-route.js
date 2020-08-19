fcd840b91301d14e243848c14afd0172dc342271
Mohammad Jangda <batmoo@gmail.com>
2020-08-19 13:30:14 -0400

    Don't do a develop build
    We expect the preflight to run on a VIP-ready version of the app. There
    are a lot of edge cases and weird behaviors with how develop-builds work
    and it's hard to account for them.
    
    Instead, let's focus on testing what's important: will a built version
    of the app work in production (with a high guarantee)?

--- preflight-checks/src/checks/02-check-port-and-healthcheck-route.js
+++ preflight-checks/src/checks/02-check-port-and-healthcheck-route.js
@@ -34,7 +34,7 @@ module.exports = {
 
 		let subprocess;
 
-		console.log( chalk.blue( '  Info:' ), `Installing dependencies with ${ chalk.yellow( 'npm install --production' ) }...` );
+		console.log( chalk.blue( '  Info:' ), `Installing production dependencies with ${ chalk.yellow( 'npm install --production' ) }...` );
 
 		return executeShell( 'npm install --production' )
 			.then( () => {
@@ -42,22 +42,14 @@ module.exports = {
 
 				console.log( chalk.blue( '  Info:' ), `Building the project using ${ chalk.yellow( buildingCommand ) }...` );
 
-				return executeShell( buildingCommand );
-			} )
-			.then( () => {
-				console.log( chalk.blue( '  Info:' ), `Removing ${ chalk.yellow( 'node_modules' ) } folder...` );
-
-				return rmfr( node_modules );
-
-			} )
-			.then( () => {
-				console.log( chalk.blue( '  Info:' ), `Installing production dependencies with ${ chalk.yellow( 'npm install --production' ) }...` );
-				return executeShell( 'npm install --production' );
+				return executeShell( buildingCommand, {
+					VIP_GO_APP_ID: '20030527',
+				} );
 			} )
 			.then( async () => {
-				console.log( chalk.blue( '  Info:' ), `Launching your app on PORT:${ PORT }...` );
+				console.log( chalk.blue( '  Info:' ), `Running ${ chalk.yellow( 'npm start' ) } to launch your app on PORT: ${ PORT }...` );
 				subprocess = executeShell( 'npm start', {
-					PORT: PORT
+					PORT: PORT,
 				} );
 				await waait( 3000 ); // Wait a little before resolving, giving time for the server to boot up
 
