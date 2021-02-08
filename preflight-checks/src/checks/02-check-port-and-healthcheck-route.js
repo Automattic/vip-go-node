41bf8ea4b52c1a939a118252f1a89ecc29c763e5
Sarosh Aga <sarosh.aga@automattic.com>
2021-02-08 19:25:38 +0530

    Adding command option to config wait time

--- preflight-checks/src/checks/02-check-port-and-healthcheck-route.js
+++ preflight-checks/src/checks/02-check-port-and-healthcheck-route.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import packageJson from '../package';
+import commandLineArgs from 'command-line-args';
 const execa = require( 'execa' );
 const waait = require( 'waait' );
 const fetch = require( 'node-fetch' );
@@ -9,11 +10,6 @@ const chalkNpmStart = chalk.yellow( 'npm start' );
 const chalkPORT = chalk.yellow( 'PORT' );
 const chalkHealthCheckRoute = chalk.yellow( CACHE_HEALTHCHECK_ROUTE );
 const chalkVIPGo = chalk.yellow( '@automattic/vip-go' );
-const rmfr = require('rmfr');
-
-const currentWorkingDirectory = process.cwd();
-const path = require( 'path' );
-const node_modules = path.join( currentWorkingDirectory, 'node_modules' );
 
 const envVariables = {
 	'VIP_GO_APP_ID': '123',
@@ -25,11 +21,16 @@ const executeShell = ( command, envVars = {} ) => {
 	} );
 }
 
+const optionDefinitions = [
+	{ name: 'wait', alias: 'w', type: Number, defaultOption: 3000 },
+];
+
+const options = commandLineArgs( optionDefinitions );
+
 module.exports = {
 	name: `Building the app and running ${ chalkNpmStart }...`,
 	excerpt: `Checking if your app accepts a ${ chalkPORT } and responds to ${ chalkHealthCheckRoute }`,
 	run: async () => {
-		const scripts = packageJson.scripts;
 		const PORT = Math.floor( Math.random() * ( 4000 - 3000 ) + 3000 ); // Get a PORT between 3000 and 4000
 
 		let subprocess;
@@ -51,7 +52,8 @@ module.exports = {
 				subprocess = executeShell( 'npm start', {
 					PORT: PORT,
 				} );
-				await waait( 3000 ); // Wait a little before resolving, giving time for the server to boot up
+
+				await waait( options.wait ); // Wait a little before resolving, giving time for the server to boot up
 
 				const cacheUrl = `http://localhost:${ PORT }${ CACHE_HEALTHCHECK_ROUTE }`;
 				console.log( chalk.blue( '  Info:' ), `Sending a GET request to ${ cacheUrl }...` );
